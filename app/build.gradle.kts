c5a4487dbf908f2ce17be059ea9d6bf9ceadf9ef
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-05-31 11:18:21 +0200

    Ajout des fichiers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
         }
     }
     namespace = "com.example.inventory"
+
 }
 
 dependencies {
@@ -66,7 +67,6 @@ dependencies {
 
     //Room
     val room_version = "2.6.1"
-
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
 
